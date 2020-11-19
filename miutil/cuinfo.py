6fa908725d24ddff2b98d7b603196e73df3dd3ac
Casper da Costa-Luis <casper.dcl@physics.org>
2020-11-19 15:27:22 +0000

    add cuinfo.get_device_count and tests

--- miutil/cuinfo.py
+++ miutil/cuinfo.py
@@ -3,11 +3,14 @@ Usage:
   miutil.cuinfo [options]
 
 Options:
-  -f, --nvcc-flags  : print out flags for use nvcc compilation
+  -c, --dev-count      : print number of devices (ignores `-d`)
+  -f, --nvcc-flags     : print out flags for use nvcc compilation
+  -d ID, --dev-id ID   : select device ID [default: None:int] for all
 """
+from argopt import argopt
 import pynvml
 
-__all__ = ["get_cc", "get_nvcc_flags"]
+__all__ = ["get_cc", "get_nvcc_flags", "get_device_count"]
 
 
 def nvmlDeviceGetCudaComputeCapability(handle):
@@ -19,25 +22,47 @@ def nvmlDeviceGetCudaComputeCapability(handle):
     return [major.value, minor.value]
 
 
-def get_cc(dev_id=0):
+def get_device_count():
+    pynvml.nvmlInit()
+    return pynvml.nvmlDeviceGetCount()
+
+
+def get_cc(dev_id=-1):
     """returns get compute capability [major, minor]"""
     pynvml.nvmlInit()
-    handle = pynvml.nvmlDeviceGetHandleByIndex(dev_id)
+    if dev_id < 0:
+        dev_id = get_device_count() + dev_id
+    try:
+        handle = pynvml.nvmlDeviceGetHandleByIndex(dev_id)
+    except pynvml.NVMLError:
+        raise IndexError("invalid dev_id")
     return tuple(nvmlDeviceGetCudaComputeCapability(handle))
 
 
-def get_nvcc_flags(dev_id=0):
-    return "-gencode=arch=compute_{0:d}{1:d},code=compute_{0:d}{1:d}".format(*get_cc())
+def get_nvcc_flags(dev_id=-1):
+    return "-gencode=arch=compute_{0:d}{1:d},code=compute_{0:d}{1:d}".format(
+        *get_cc(dev_id)
+    )
 
 
 def main(*args, **kwargs):
-    from argopt import argopt
-
     args = argopt(__doc__).parse_args(*args, **kwargs)
+    noargs = True
+    devices = range(get_device_count()) if args.dev_id is None else [args.dev_id]
+
+    if args.dev_count:
+        print(get_device_count())
+        noargs = False
     if args.nvcc_flags:
-        print(get_nvcc_flags())
-    else:
-        print("Compute capability: {:d}.{:d}".format(*get_cc()))
+        print(" ".join(sorted(set(map(get_nvcc_flags, devices)))[::-1]))
+        noargs = False
+    if noargs:
+        for dev_id in devices:
+            print(
+                "Device {:2d}:compute capability:{:d}.{:d}".format(
+                    dev_id, *get_cc(dev_id)
+                )
+            )
 
 
 if __name__ == "__main__":
--- tests/test_cuinfo.py
+++ tests/test_cuinfo.py
@@ -1,23 +1,43 @@
-from pytest import importorskip
+from pytest import importorskip, raises
 
 importorskip("pynvml")
+from miutil.cuinfo import get_cc, get_device_count  # NOQA: E402
 
 
 def test_get_cc():
-    from miutil.cuinfo import get_cc
-
     cc = get_cc()
     assert len(cc) == 2, cc
     assert all(isinstance(i, int) for i in cc), cc
 
 
+def test_get_device_count():
+    devices = get_device_count()
+    assert isinstance(devices, int)
+
+
 def test_cuinfo_cli(capsys):
     from miutil.cuinfo import main
 
+    main(["--dev-count"])
+    out, _ = capsys.readouterr()
+    devices = int(out)
+    assert devices >= 0
+
+    # individual dev_id
+    for dev_id in range(devices):
+        main(["--dev-id", str(dev_id)])
+        out, _ = capsys.readouterr()
+        assert len(out.split("Device ")) == devices + 1
+
+    # all dev_ids
     main()
-    out, err = capsys.readouterr()
-    assert out.startswith("Compute capability:")
+    out, _ = capsys.readouterr()
+    assert len(out.split("Device ")) == devices + 1
+
+    # dev_id one too much
+    with raises(IndexError):
+        main(["--dev-id", str(devices)])
 
     main(["--nvcc-flags"])
-    out, err = capsys.readouterr()
-    assert out.startswith("-gencode=")
+    out, _ = capsys.readouterr()
+    assert not devices or out.startswith("-gencode=")
