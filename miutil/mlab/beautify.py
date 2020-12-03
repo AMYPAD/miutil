973f5f19b0abe53f52c13b4b2755c30510ee88f6
Casper da Costa-Luis <casper.dcl@physics.org>
2020-12-03 14:11:20 +0000

    update mbeautify
    - https://github.com/davidvarga/MBeautifier/pull/97

--- miutil/mlab/beautify.py
+++ miutil/mlab/beautify.py
@@ -17,7 +17,7 @@ from ..web import get_file
 from . import get_engine
 
 log = logging.getLogger(__name__)
-MBEAUTIFIER_REV = "73bc3da7df39a60aab859a2cb86dd82b4d284312"
+MBEAUTIFIER_REV = "9c3c82387ec3c0fb29e707ebd060e8e2ca9ea6f2"
 
 
 @lru_cache()
@@ -26,7 +26,7 @@ def ensure_mbeautifier(*args, **kwargs):
     eng = get_engine(*args, **kwargs)
     fn = get_file(
         "MBeautifier-%s.zip" % MBEAUTIFIER_REV[:7],
-        "https://github.com/AMYPAD/MBeautifier/archive/%s.zip" % MBEAUTIFIER_REV,
+        "https://github.com/davidvarga/MBeautifier/archive/%s.zip" % MBEAUTIFIER_REV,
     )
     outpath = path.join(path.dirname(fn), "MBeautifier-%s" % MBEAUTIFIER_REV)
     if not path.exists(outpath):
